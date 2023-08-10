b8c0a6fa19c53576dcc2a8ddc61446d3ab144a7b
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-08-10 11:06:54 -0300

    usando o forEach para retornar os videos na tela

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,10 +1,10 @@
 async function listaVideos() {
-  const conexao = await fetch(' http://localhost:3000/videos');
+  const conexao = await fetch('http://localhost:3000/videos');
   const conexaoConvertida = await conexao.json();
-  
+
   return conexaoConvertida;
 }
 
-export const conectaAPI = {
+export const conectaApi = {
   listaVideos
 }
